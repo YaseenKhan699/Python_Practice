5761ed2642bd283d9aef20b369ca076de587e6be
Macbook Pro <macbookpro@192.168.1.6>
2023-10-08 22:46:30 +0500

    User email and pasword login page practice on if else

---
+++ prac_ifelse.py
@@ -0,0 +1,29 @@
+#email = yaseen@gmail.com
+#password = 11222233
+email = input('Enter your email\n')
+if email== 'yaseen@gmail.com' and '@' in email:
+    print('Correct email')
+else : print('Incorrect please re-enter your email')
+
+email = input()
+
+if email== 'yaseen@gmail.com' and '@' in email :
+        print('Finally Correct email welcome')
+        password = input('Enter your password\n')
+        if password== 'yaseen@gmail.com':
+            print('Correct password')
+        else : print('Incorrect please re-enter your password')
+
+        password = input()
+
+        if password== '11222233' :
+                print('Finally Correct passworddd welcome')
+        else:
+            print('try again please')
+else:
+     print('try again please')
+  
+  
+  #password section 
+
+  
